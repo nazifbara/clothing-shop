89a4f62a2aa497a4d8452bce0a8eab721660fae7
nazifbara <nazifbarassounon@gmail.com>
2021-07-21 17:28:23 +0100

    Style GameList with SASS

--- src/components/game_list/game_list.jsx
+++ src/components/game_list/game_list.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './game_list.css';
 
 function GameList({ list, children }) {
   return (
     <div className="Container">
-      <section className="GameList">{list.map((g) => children(g))}</section>
+      <section className="game-list">{list.map((g) => children(g))}</section>
     </div>
   );
 }
