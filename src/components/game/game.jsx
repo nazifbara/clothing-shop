4b6a071c13a0f8482d8cfdef03d8228f0c9f7cff
nazifbara <nazifbarassounon@gmail.com>
2021-07-21 17:16:24 +0100

    Style Game component with SASS

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Image from 'react-graceful-image';
-import './game.css';
 import { printPrice } from '../../utils';
 import AddToCart from '../add_to_cart';
 
 function Game({ item }) {
   return (
-    <article className="Game" key={item.id}>
+    <article className="game" key={item.id}>
       <Image width="100%" alt="" src={item.imageUrl} />
       <h3>{item.title}</h3>
       <span>{printPrice(item.price)}</span>
