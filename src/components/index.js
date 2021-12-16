7a825062cdbfd17c045d2c5dabb0d75c21e0e1d8
nazifbara <nazifbarassounon@gmail.com>
2021-12-16 17:29:35 +0100

    export using index

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 export { default as AddToCart } from './AddToCart';
-export { default as Button } from './Button/Button';
+export { default as Button } from './Button';
 export { default as Cart } from './Cart';
 export { default as IconButton } from './IconButton';
 export { default as ImageBox } from './ImageBox';
-export { default as Message } from './Message/Message';
+export { default as Message } from './Message';
 export { default as ProductCard } from './ProductCard';
 export { default as ProductList } from './ProductList';
